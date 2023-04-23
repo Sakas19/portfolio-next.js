c5fc4a9390f55c3320cff7c5a360cfe96113bed6
Sarvnaz K <sarvnaz.kasaei@edu.cmeducations.se>
2023-04-23 10:23:35 +0200

    added 404

--- app/404.js
+++ app/404.js
@@ -1,15 +1,13 @@
-import Link from 'next/link'
+import React from 'react';
 
 const Custom404 = () => {
   return (
     <div>
       <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <Link href="/">
-        <a>Go back to the homepage</a>
-      </Link>
+      <p>Sorry, the page you are looking for cannot be found.</p>
     </div>
-  )
-}
+  );
+};
 
-export default Custom404
+export default Custom404;
+
