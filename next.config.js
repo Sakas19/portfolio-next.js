4e1f3fdace970231fb82c00cb03c2a69520a0afc
Sarvnaz K <sarvnaz.kasaei@edu.cmeducations.se>
2023-06-22 10:57:13 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Commented out the experimental configuration
+// next.config.js
+module.exports = {
   experimental: {
     appDir: true,
   },
-}
-
-module.exports = nextConfig;
+};
